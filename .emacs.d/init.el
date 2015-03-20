63ae17e1c0883d1b49d2ae3f8a2913c799776083
kohey18 <k.kouhei18@gmail.com>
2015-03-20 20:00:56 +0900

    add color-theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -47,3 +47,16 @@
 (set-face-foreground 'whitespace-space "yellow")
 (set-face-background 'whitespace-space "red")
 (set-face-underline  'whitespace-space t)
+
+;; click board
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
---
+++ .emacs.d/inits/24-color-theme-molokai.el
@@ -0,0 +1,2 @@
+(load "~/dotfiles/.emacs.d/.cask/24.4.1/elpa/color-theme-molokai-0.1/color-theme-molokai.el")
+(color-theme-molokai)
