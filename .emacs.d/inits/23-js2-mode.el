b3b74765d20dd36f49792c85c70cc6eac379c3a3
kohey18 <k.kouhei18@gmail.com>
2015-03-20 20:31:26 +0900

    add ac-js2-mode

--- .emacs.d/inits/23-js2-mode.el
+++ .emacs.d/inits/23-js2-mode.el
@@ -1,3 +1,5 @@
 (require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (setq-default tab-width 4 indent-tabs-mode nil)
+
+(add-hook 'js2-mode-hook 'ac-js2-mode)
--- .emacs.d/inits/23-ruby.el
+++ .emacs.d/inits/23-ruby.el
@@ -10,11 +10,11 @@
 ;; --------------------------------------------------
 (require 'ruby-end)
 (add-hook 'ruby-mode-hook
-	  '(lambda ()
-	     (abbrev-mode 1)
-	     (electric-pair-mode t)
-	     (electric-indent-mode t)
-	     (electric-layout-mode t)))
+          '(lambda ()
+             (abbrev-mode 1)
+             (electric-pair-mode t)
+             (electric-indent-mode t)
+             (electric-layout-mode t)))
 
 
 ;; --------------------------------------------------
